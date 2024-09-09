96066642d45ca9d6e236df3d98c4069bfdf7fcda
Abdelmalek1-acrh <malekchrifialaoui513@gmail.com>
2024-09-09 14:53:30 +0200

    Add files via upload

---
+++ calculater/java.js
@@ -0,0 +1,57 @@
+// CALCULATOR PROGRAM
+
+
+
+const display = document.getElementById("display");
+
+function appendtodisplay(input){
+    appendtodisplay.value += input;
+}
+
+function cleardisplay(){
+
+}
+
+
+function calculate(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ calculater/javascript/java.js
@@ -0,0 +1,57 @@
+// CALCULATOR PROGRAM
+
+
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    appendToDisplay.value += input;
+}
+
+function cleardisplay(){
+
+}
+
+
+function calculate(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
